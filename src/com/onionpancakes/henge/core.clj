5072278007891d06af43b0895040a78fe7dd40f3
Gordon Lin <gordonlin89@gmail.com>
2018-12-17 22:03:30 -0800

    Impl compile

--- src/com/onionpancakes/henge/core.clj
+++ src/com/onionpancakes/henge/core.clj
@@ -1,5 +1,7 @@
 (ns com.onionpancakes.henge.core
+  (:refer-clojure :exclude [compile])
   (:require [clojure.spec.alpha :as spec]
+            [clojure.spec.gen.alpha :as gen]
             [clojure.walk]))
 
 (spec/def ::element-seq
@@ -13,9 +15,75 @@
             (complement map-entry?)
             ::element-seq))
 
+(def ^:dynamic *create-element-fn*
+  `js/React.createElement)
+
+(spec/def ::create-element-seq
+  (spec/cat ::fn #{*create-element-fn*}
+            ::type any?
+            ::props (spec/? any?)
+            ::children (spec/* ::form)))
+
+(defn create-element-form-gen []
+  (->> (spec/gen list?)
+       (gen/fmap (partial cons *create-element-fn*))))
+
+(spec/def ::create-element-form
+  (spec/and
+   (spec/coll-of any? :kind seq?
+                 :min-count 2
+                 :gen create-element-form-gen)
+   ::create-element-seq))
+
 (spec/def ::form
   (spec/or ::element ::element-form
+           ::create-element ::create-element-form
            ::map (spec/map-of any? ::form)
            ::coll (spec/coll-of ::form)
            ::other any?))
+
+(defn component-tag?
+  [k]
+  (Character/isUpperCase (first (name k))))
+
+(defn keyword->symbol
+  [k]
+  (symbol (namespace k) (name k)))
+
+(defn tag->type
+  [k]
+  (if (component-tag? k)
+    (keyword->symbol k)
+    (name k)))
+
+(defmulti process-node first)
+
+(defmethod process-node ::element
+  [[_ m]]
+  [::create-element (merge m {::fn   *create-element-fn*
+                              ::type (tag->type (::tag m))})])
+
+(defmethod process-node :default
+  [node]
+  node)
+
+(defn node?
+  [x]
+  (and (vector? x) (= (count x) 2)))
+
+(defn process
+  [x]
+  (if (node? x)
+    (process-node x)
+    x))
+
+(defn compile*
+  [form]
+  (->> (spec/conform ::form form)
+       (clojure.walk/postwalk process)
+       (spec/unform ::form)))
+
+(defmacro compile
+  [form]
+  (compile* form))
 
