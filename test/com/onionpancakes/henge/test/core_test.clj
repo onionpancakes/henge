ac92657281bbb5bc322dac957aec917954dfe21c
Gordon Lin <gordonlin89@gmail.com>
2018-12-17 16:48:15 -0800

    Refactored unit tests

--- test/com/onionpancakes/henge/test/core_test.clj
+++ test/com/onionpancakes/henge/test/core_test.clj
@@ -3,29 +3,29 @@
             [clojure.test :refer [deftest is are]]
             [clojure.spec.alpha :as spec]))
 
-(deftest test-unform-element
-  (are [x y] (let [res (spec/unform ::h/element-form x)]
-               (and (= res y) (vector? res)))
-    {::h/tag   :foo
-     ::h/props nil}                [:foo nil]
-    {::h/tag   :foo
-     ::h/props :bar}               [:foo :bar]
-    {::h/tag      :foo
-     ::h/props    :bar
-     ::h/children [[::h/other :baz]]} [:foo :bar :baz]))
+(def form->conformed
+  {[:foo]          [::h/element #::h{:tag :foo}]
+   [:foo nil]      [::h/element #::h{:tag   :foo
+                                     :props nil}]
+   [:foo nil :bar] [::h/element #::h{:tag      :foo
+                                     :props    nil
+                                     :children [[::h/other :bar]]}]
+   '([:foo])       [::h/coll
+                    '([::h/element #::h{:tag :foo}])]
+   #{:foo}         [::h/coll #{[::h/other :foo]}]
+   {:foo [:foo]}   [::h/map
+                    {:foo '[::h/element #::h{:tag :foo}]}]
+   :foo            [::h/other :foo]})
 
-(deftest test-conform-unform-identical
-  (are [x] (->> (spec/conform ::h/form x)
-                (spec/unform ::h/form)
-                (= x))
-    nil
-    :foo
-    '()
-    []
-    {}
-    #{}
-    {:foo :bar}
-    [:foo nil]
-    '(foo)
-    '(foo [:foo {} '(bar [:bar nil #{}])])))
+(deftest test-conform
+  (doseq [[form conformed] form->conformed]
+    (is (= (spec/conform ::h/form form) conformed))))
+
+(deftest test-unform
+  (doseq [[form conformed] form->conformed]
+    (is (= (spec/unform ::h/form conformed) form))))
+
+(deftest test-unform-element-vector
+  (are [x] (vector? (spec/unform ::h/form x))
+    [::h/element #::h{:tag :foo}]))
 
