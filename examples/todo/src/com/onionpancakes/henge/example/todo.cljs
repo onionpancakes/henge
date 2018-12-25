c8604862371c8ca4e5b56a348c3928312f8cfa45
Gordon Lin <gordonlin89@gmail.com>
2018-12-25 15:07:46 -0800

    Refactored todo; added styles and README

--- examples/todo/src/com/onionpancakes/henge/example/todo.cljs
+++ examples/todo/src/com/onionpancakes/henge/example/todo.cljs
@@ -4,29 +4,59 @@
 (defn TodoInput [props]
   (let [[st st!]      (js/React.useState "")
         handleNewTask (.-handleNewTask props)
+        handleClear   (.-handleClear props)
         input-ref     (js/React.createRef)]
     (t/compile
-     [:div nil
-      [:input {:onChange #(st! (.. % -target -value))
-               :value    st
-               :ref      input-ref}]
-      [:button {:onClick #(do
-                            (handleNewTask st)
-                            (st! "")
-                            (.. input-ref -current focus))}
-       "Add Task"]])))
+     [:div :.TodoInput
+      [:div nil
+       [:input {:value    st
+                :ref      input-ref
+                :onChange #(st! (.. % -target -value))
+                :onKeyPress #(when (= (.-key %) "Enter")
+                               (handleNewTask st)
+                               (st! ""))}]
+       [:button {:onClick #(when (not (= st ""))
+                             (handleNewTask st)
+                             (st! "")
+                             (.. input-ref -current focus))}
+        "Add Task"]
+       [:button {:onClick handleClear} "Clear"]]])))
+
+(defn TodoTask [props]
+  (let [text       (.-text props)
+        done?      (.-done props)
+        handleDone (.-handleDone props)]
+    (t/compile
+     [:div {::t/classes {:TodoTask true
+                         :done     done?}}
+      [:label nil
+       [:input {:type     "checkbox"
+                :checked  done?
+                :onChange handleDone}]
+       text]])))
 
 (defn TodoApp [_]
   (let [[st st!]      (js/React.useState [])
-        handleNewTask #(->> {:id (random-uuid) :task %}
+        handleNewTask #(->> {:id (random-uuid) :text % :done? false}
                             (conj st)
+                            (st!))
+        handleClear   #(->> (remove :done? st)
+                            (into [])
                             (st!))]
     (t/compile
-     [:div nil
-      [:TodoInput {:handleNewTask handleNewTask}]
-      [:ol nil
-       (for [{:keys [id task]} st]
-         [:li {:key id} task])]])))
+     [:div :.TodoApp
+      [:h1 nil "Todo"]
+      [:div nil
+       (if (not (empty? st))
+         (for [[idx {:keys [id text done?]}] (map-indexed vector st)]
+           [:TodoTask {:key        id
+                       :text       text
+                       :done       done?
+                       :handleDone #(st! (update-in st [idx :done?] not))}])
+         [:i :.TodoApp-placeholder
+          "What needs to be done?"])]
+      [:TodoInput {:handleNewTask handleNewTask
+                   :handleClear   handleClear}]])))
 
 (js/ReactDOM.render (t/compile [:TodoApp])
                     (js/document.getElementById "app"))
